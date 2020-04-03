ed9d17ec241bc5ada63fa68334ca134eb1bf3848
Meta (X230T) <crabiter@vivaldi.net>
2020-04-03 17:34:16 +0300

    Account for both HasExtras and Shadow flags

--- src/ReplaceShpShadowCheck.asm
+++ src/ReplaceShpShadowCheck.asm
@@ -1,9 +1,10 @@
 %include "macros/patch.inc"
 
 @LJMP    0x423358, ReplaceShpShadowCheck
-ReplaceShpShadowCheck:        ; Replace checking of unknown field with checking of Shadow flag
+ReplaceShpShadowCheck:        ; Replace checking of HasExtras with checking of HasExtras && Shadow
      push edx                 ; Store previously used register value
      mov  edx, [esi+0C8h]     ; AnimClass->AnimType
      mov  al, [edx+372h]      ; AnimType->Shadow
+     and  al, [esi+194h]      ; && AnimClass->HasExtras
      pop  edx                 ; Restore previously used register value
      jmp  0x42335E            ; Return to next instruction
